e8c4e7a3fd78e0eed6be99f964a32b88b4831bd3
yarden-simplee <yarden@simplee.com>
2018-08-11 20:56:51 +0300

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,17 @@
 class StaticPagesController < ApplicationController
+	
+  # For each action, the controller will perform the action
+  # and then render the corresponding view by name.
   def home
   end
 
   def help
   end
+
+  def about
+  end
+
+  def contact
+  end
+
 end
