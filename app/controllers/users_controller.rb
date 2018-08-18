da34c868b1f971d5600476dfe22034bd9de420a0
yarden-simplee <yarden@simplee.com>
2018-08-18 20:44:27 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  def show
+  	@user = User.find(params[:id]) # The 'id' param is supplied in the url
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)   
+    if @user.save
+    	flash[:success] = "Welcome to the Sample App!" # For later disdplay of a temp message in case of success
+      redirect_to @user # Equivalent to 'redirect_to user_url(@user)'. Rails automatically infers the latter from the former
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  # Returns a version of the 'params' hash with only the specified parameters
+  # If a oarameter named 'user' is not present, an error is raised
+  def user_params
+  	params.require(:user).permit(:name, :email, :password,
+  				   :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   get '/about', to:'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users # This will make rails acknowledge requests made for Url '/users/id' as well as all other restful actions for the user resource
 
 end
