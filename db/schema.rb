08748562661397d1f689cfc8f505a908a9ffe6a0
yarden-simplee <yarden@simplee.com>
2018-09-19 20:16:49 +0300

    Resolved two migrations with add_reset_to_users name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180918094744) do
 
   create_table "users", force: :cascade do |t|
